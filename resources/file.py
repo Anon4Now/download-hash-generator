bf1199eced0bf4d5c1d396befc3b8c8497381295
Anon4Now <tutkoda@gmail.com>
2022-09-03 14:09:53 -0400

    added the static methods that overwrite the PatternMatchingEventHandler class

--- resources/file.py
+++ resources/file.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 
 # Third-party imports
 from watchdog.observers import Observer
-
+from watchdog.events import PatternMatchingEventHandler
 
 
 @dataclass
@@ -23,4 +23,25 @@ class File:
         :_my_event_handler: Setting the event handler config to check for all events (https://python-watchdog.readthedocs.io/en/stable/api.html#watchdog.observers.Observer)
         :return: None
         """
+        """Event Handler Configs"""
+        _my_event_handler = PatternMatchingEventHandler(patterns=["*"])
+        _my_event_handler.on_created = self.on_created_event  # overriding methods in PatternMatchingEventHandler class
+        _my_event_handler.on_modified = self.on_modified_event  # overriding methods in PatternMatchingEventHandler class
 
+        """Observer Configs/Start"""
+        _my_observer = Observer()
+        _my_observer.schedule(
+            event_handler=_my_event_handler,
+            path=self.path,
+            recursive=True
+        )
+        _my_observer.start()
+
+    @staticmethod
+    def on_created_event(event) -> bool:
+        print(event)
+        return True
+
+    @staticmethod
+    def on_modified_event(event) -> None:
+        pass
