995073176839124f21e482d62d056e4b349b66ad
Anon4Now <tutkoda@gmail.com>
2022-09-03 11:51:41 -0400

    created file.py and added default utils file

---
+++ resources/utils.py
@@ -0,0 +1,89 @@
+"""Module containing Utility Functions"""
+
+# Standard Library imports
+import logging
+import optparse
+
+
+#####################################
+# Get Environment Variables
+#####################################
+# LOGGING_LEVEL ----
+
+#####################################
+# Create logger func
+#####################################
+def create_logger() -> logging:
+    """
+    Create a logger
+    :return: logger
+    """
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s: %(levelname)s: %(message)s')
+    log = logging.getLogger()
+    # log.setLevel(logging.INFO)
+
+    logging.getLogger('boto').setLevel(logging.CRITICAL)
+    logging.getLogger('botocore').setLevel(logging.CRITICAL)
+    return log
+
+
+logger = create_logger()  # create logger func
+
+
+###########################
+# Custom Error Handler func
+###########################
+
+def error_handler(func):
+    # exception handling decorator function
+
+    def inner_func(*args, **kwargs):
+        try:
+            result = func(*args, **kwargs)
+            return result
+        except botocore.exceptions.NoCredentialsError as err:
+            logger.error("NoCredentialsError: error=%s func=%s", err.fmt, func.__name__)
+        except botocore.exceptions.NoRegionError as err:
+            logger.error("NoRegionError: error=%s func=%s", err.fmt, func.__name__)
+        except botocore.exceptions.ClientError as err:
+            logger.error("ClientError: error=%s func=%s", err, func.__name__)
+        except Exception as err:
+            logger.error("GeneralException: error=%s func=%s", err, func.__name__)
+
+    return inner_func
+
+
+#######################################
+# Option Parser
+#######################################
+parser = optparse.OptionParser()
+
+
+def getArgs():
+    parser.add_option(
+        "-o",
+        "--option",
+        dest="sanitize_option",
+        help="Requires either (delete OR modify)"
+             "** delete option **\n"
+             "- Deletes Internet Gateway"
+             "- Deletes Subnets"
+             "- Deletes Route Tables (not default)"
+             "- Deletes NACL (not default)"
+             "- Deletes SG (not default)"
+             "- Deletes default VPC"
+             "- Updates SSM parameter preferences to block public access\n\n"
+             "** modify option **\n"
+             "- Updates default NACL (removes inbound/outbound rules)"
+             "- Updates default SG (removes inbound/outbound rules)"
+             "- Updates SSM parameter preferences to block public access"
+    )
+    parsingInput = parser.parse_args()
+
+    (options, args) = parsingInput
+
+    if not options.sanitize_option:
+        parser.error("[-] Please specify an option flag, --help for more info")
+    else:
+        return options
+
