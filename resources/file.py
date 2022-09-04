954c5fcfd88d4bf7ef63a69c06bb50793bf95d71
Anon4Now <tutkoda@gmail.com>
2022-09-04 15:12:09 -0400

    added method to File class

--- resources/file.py
+++ resources/file.py
@@ -1,7 +1,7 @@
 """Module containing the Class for handling File changes"""
 
 # Standard Library imports
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 # Third-party imports
 from watchdog.observers import Observer
@@ -11,6 +11,7 @@ from watchdog.events import PatternMatchingEventHandler
 @dataclass
 class File:
     path: str
+    _my_event_handler: object = field(default_factory=object, repr=False)
     """Class that contains attrs and methods related
     to the tracking and event raising events around
     file changes in the monitored path
@@ -24,18 +25,9 @@ class File:
         :return: None
         """
         """Event Handler Configs"""
-        _my_event_handler = PatternMatchingEventHandler(patterns=["*"])
-        _my_event_handler.on_created = self.on_created_event  # overriding methods in PatternMatchingEventHandler class
-        _my_event_handler.on_modified = self.on_modified_event  # overriding methods in PatternMatchingEventHandler class
-
-        """Observer Configs/Start"""
-        _my_observer = Observer()
-        _my_observer.schedule(
-            event_handler=_my_event_handler,
-            path=self.path,
-            recursive=True
-        )
-        _my_observer.start()
+        self._my_event_handler = PatternMatchingEventHandler(patterns=["*"])
+        self._my_event_handler.on_created = self.on_created_event  # overriding methods in PatternMatchingEventHandler class
+        self._my_event_handler.on_modified = self.on_modified_event  # overriding methods in PatternMatchingEventHandler class
 
     @staticmethod
     def on_created_event(event) -> bool:
@@ -45,3 +37,14 @@ class File:
     @staticmethod
     def on_modified_event(event) -> None:
         pass
+
+    def start_observer(self) -> bool:
+        """Observer Configs/Start"""
+        _my_observer = Observer()
+        _my_observer.schedule(
+            event_handler=self._my_event_handler,
+            path=self.path,
+            recursive=True
+        )
+        _my_observer.start()
+        return True
