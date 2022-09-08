9e6e54cbfab3266e1758c9ae54bce3eac612086d
Anon4Now <tutkoda@gmail.com>
2022-09-08 10:11:54 -0400

    refactored code to be dataclass, confirmed working

--- resources/vt_check.py
+++ resources/vt_check.py
@@ -1,60 +1,35 @@
-# import requests, os, datetime
-# from dotenv import load_dotenv
-#
-#
-# # Run SHA256 against Virus Total DB
-# class VTChecking:
-#     load_dotenv()  # load .env file containing vars
-#
-#     # Set env vars during object instantiation
-#     def __init__(self):
-#         self.API_ENDPOINT = os.getenv('API_ENDPOINT')
-#         self.API_KEY = os.getenv('API_KEY')
-#         self.API_KEY_VAL = os.getenv('API_KEY_VAL')
-#
-#     # Use GET to check to see if data exists on hash
-#     def hashExists(self, checkHash):
-#         # See if file has already been uploaded
-#         url = f"{self.API_ENDPOINT}{checkHash}"
-#
-#         headers = {
-#             "Accept": "application/json",
-#             self.API_KEY: self.API_KEY_VAL}
-#         response = requests.request("GET", url, headers=headers)
-#         return response.text
-#
-#     # Parse the response from VT API
-#     @staticmethod
-#     def parseJson(content):
-#         lastStatsDict = {}  # empty dict to use as data store
-#
-#         # extract useful data from JSON response
-#         extractLastAnalysisDate = datetime.datetime.fromtimestamp(
-#             content['data']['attributes']['last_analysis_date'])
-#         extractLastAnalysisStats = content['data']['attributes']['last_analysis_stats']
-#         print(f'Last Analysis Date : {extractLastAnalysisDate}')
-#         print(f'Last Analysis Stats:')
-#         for key in extractLastAnalysisStats:
-#             lastStatsDict[key] = extractLastAnalysisStats[key]
-#             print(F'   {key}: {extractLastAnalysisStats[key]}')
-#
-#         # make determination based on content
-#         for key in lastStatsDict:
-#             if key == 'malicious':
-#                 if lastStatsDict[key] == 0:
-#                     print(f'\nBased on results, the file appears safe')
-#                 elif 0 < lastStatsDict[key] < 2:
-#                     print(f'\nSome vendors flagged this file, consider additional research')
-#                 elif lastStatsDict[key] > 2:
-#                     print(f'\nThis file may be malicious, consider not downloading')
-#
-#     # If error exists in JSON response, parse the error
-#     @staticmethod
-#     def checkError(content):
-#         extractErrorCode = content['error']['code']
-#         if extractErrorCode == 'NotFoundError':
-#             print(
-#                 f'This file has not been uploaded to Virus Total before -- consider uploading the file for scanning at \n"https://www.virustotal.com/gui/home/upload"')
-#
-#         elif extractErrorCode != 'NotFoundError':
-#             print(f'Error returned from VT is {extractErrorCode}')
+import requests
+import json
+import datetime
+from dataclasses import dataclass, field
+
+
+@dataclass
+class VirusTotal:
+    hash_sha256: str
+    api_endpoint: str
+    api_key: str
+    api_key_val: str
+    out_dict: dict = field(default_factory=dict, init=False, repr=False)
+
+    def __post_init__(self):
+        if not self.response.get('error'):
+            _analysis_date = datetime.datetime.fromtimestamp(
+                self.response.get('data').get('attributes').get('last_analysis_date'))
+            self.out_dict['LastAnalysisDate'] = _analysis_date
+            _analysis_stats = self.response.get('data').get('attributes').get('last_analysis_stats')
+            self.out_dict['LastAnalysisStats'] = _analysis_stats
+        else:
+            _error_code = self.response.get('error').get('code')
+            self.out_dict['error_code'] = _error_code
+
+    @property
+    def response(self) -> dict:
+        # See if file has already been uploaded
+        url = f"{self.api_endpoint}{self.hash_sha256}"
+
+        headers = {
+            "Accept": "application/json",
+            self.api_key: self.api_key_val}
+        response = requests.request("GET", url, headers=headers).text
+        return json.loads(response)
