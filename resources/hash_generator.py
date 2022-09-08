820e5ca0ee5e6e493eb338d0f6a3e624682ff60b
Anon4Now <tutkoda@gmail.com>
2022-09-07 23:08:43 -0400

    refactored to a dataclass

--- resources/hash_generator.py
+++ resources/hash_generator.py
@@ -1,44 +1,34 @@
+from dataclasses import dataclass, field
 import hashlib
 
 
-# Generate Hashes from file data
-class HashGenerator:
-    hash_sha256 = hashlib.sha256()  # Create the hash object for SHA256 `.sha256()`
-    hash_sha1 = hashlib.sha1()  # Create the hash object for SHA1 `.sha1()`
-    hash_md5 = hashlib.md5()  # Create the hash object for MD5`.md5()'
+@dataclass
+class Hash:
+    """Data-oriented class"""
+    text_file: str
+    text_data: str = field(default_factory=str, init=False, repr=False)
+    hash_sha256: str = field(default_factory=str, init=False, repr=False)
+    hash_sha1: str = field(default_factory=str, init=False, repr=False)
+    hash_md5: str = field(default_factory=str, init=False, repr=False)
 
-    def __init__(self, fileName):
-        self.fileName = fileName  # get filename with data during object instatiation
-        self.data = None
+    def __post_init__(self):
+        with open(self.text_file) as f:
+            self.text_data = f.read()
+        self._generate_hashes()
 
-    # Open and read file containing file name, update instance var
-    def readFile(self):
-        with open(self.fileName) as file:
-            self.data = file.read()
-            print(f'[File Name] - {self.data}')
-
-    # Read the size of the file by Block Size in buffer, generate hashes
-    def generateHash(self):
+    def _generate_hashes(self) -> None:
         BLOCK_SIZE = 65536  # The size of each read from the file
+        sha_256 = hashlib.sha256()
+        sha_1 = hashlib.sha1()
+        md_5 = hashlib.md5()
 
-        with open(self.data, 'rb') as f:  # Open the file to read its bytes
+        with open(self.text_data, 'rb') as f:  # Open the file to read its bytes
             fb = f.read(BLOCK_SIZE)  # Read from the file. Take in the amount declared above
             while len(fb) > 0:  # While there is still data being read from the file
-                HashGenerator.hash_sha256.update(fb)  # Update the hash
-                HashGenerator.hash_sha1.update(fb)  # Update the hash
-                HashGenerator.hash_md5.update(fb)  # Update the hash
+                sha_256.update(fb)  # Update the hash
+                sha_1.update(fb)
+                md_5.update(fb)
                 fb = f.read(BLOCK_SIZE)  # Read the next block from the file
-
-    # Return/Print hash values
-    @staticmethod
-    def returnHashes():
-        print(f'[SHA-256 Hash] - {HashGenerator.hash_sha256.hexdigest()}')  # Print the hash value to CLI
-        print(f'[SHA-1 Hash] - {HashGenerator.hash_sha1.hexdigest()}')  # Print the hash value to CLI
-        print(f'[MD5 Hash] - {HashGenerator.hash_md5.hexdigest()}\n')  # Print the hash value to CLI
-        return HashGenerator.hash_sha256.hexdigest()
-
-    # Runs the methods
-    def startHash(self):
-        self.readFile()
-        self.generateHash()
-        return self.returnHashes()
+        self.hash_sha256 = str(sha_256.hexdigest())
+        self.hash_sha1 = str(sha_1.hexdigest())
+        self.hash_md5 = str(md_5.hexdigest())
