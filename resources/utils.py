3a2f219fcd79bf94dc542ee837791d81e85063d1
Anon4Now <tutkoda@gmail.com>
2022-09-09 20:16:33 -0400

    added comments to file

--- resources/utils.py
+++ resources/utils.py
@@ -3,14 +3,32 @@
 # Standard Library imports
 import logging
 
+# Third-party imports
+from dotenv import load_dotenv, find_dotenv
 from watchdog.events import PatternMatchingEventHandler
 from watchdog.observers import Observer
+
+
+temp_file = 'temp.txt'  # name of the temp file created and deleted by script
 
 
 #####################################
 # Get Environment Variables
 #####################################
 # LOGGING_LEVEL ----
+
+# DOTENV ENVIRONMENT VARIABLES
+def get_envs() -> bool:
+    """
+    A function that checks that a dotenv file exists in the dir,
+    and if yes it will load the env vars.
+    :return: Boolean result to depict whether the dotenv file exists (i.e. True = exists / False = doesn't exist)
+    """
+    if find_dotenv():
+        load_dotenv()
+        return True
+    else:
+        return False
 
 
 #####################################
@@ -59,23 +77,37 @@ logger = create_logger()  # create logger func
 # Watchdog File Management Funcs
 #####################################
 def on_modified_event(event) -> None:
+    """
+    Function that overwrites a method of the same name in the PatternMatchingEventHandler class.
+    Used to pass events from the watcher and perform actions on "modify" events.
+    ** IMPORTANT ** -- Modifying this function will change the behavior of the whole program
+    :param event: An event in the form of a class, used to write info to temp_file.txt
+    :return: None
+    """
     get_event = str(event.src_path)  # get the src_path from event dict
     if 'exe' in get_event:
-        with open("exe.txt", "w+") as file:  # write to the file for every modified event that is recieved
+        with open(temp_file, "w+") as file:  # write to the file for any matching modified event that is received
             file.write(get_event)
 
 
+# Needed vars and assignments for watcher to function properly
 my_event_handler: PatternMatchingEventHandler = PatternMatchingEventHandler(patterns=["*"])
 my_event_handler.on_modified = on_modified_event  # overriding methods in PatternMatchingEventHandler class
 
 
 def start_observer(my_observer: Observer) -> None:
+    """
+    Function to start the watcher, which calls a method on the Observer class
+    :param my_observer: Instantiated instance of the Observer class from main.py
+    :return: None
+    """
     my_observer.start()
 
 
 def stop_observer(my_observer: Observer) -> None:
+    """
+    Function to stop the watcher, which calls a method on the Observer class
+    :param my_observer: Instantiated instance of the Observer class from main.py
+    :return: None
+    """
     my_observer.stop()
-
-
-
-
