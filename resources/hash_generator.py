16c1da67aef452f6c1ede51563a1fa03cb8d6d82
Anon4Now <tutkoda@gmail.com>
2022-09-09 21:05:58 -0400

    added error handling to class

--- resources/hash_generator.py
+++ resources/hash_generator.py
@@ -4,6 +4,9 @@
 from dataclasses import dataclass, field
 import hashlib
 
+# Local App imports
+from resources.utils import error_handler
+
 
 @dataclass
 class Hash:
@@ -18,6 +21,7 @@ class Hash:
     hash_sha1: str = field(default_factory=str, init=False, repr=False)
     hash_md5: str = field(default_factory=str, init=False, repr=False)
 
+    @error_handler
     def __post_init__(self) -> None:
         """
         This post init does two things:
@@ -25,11 +29,15 @@ class Hash:
         2. It will call the _generate_hashes method to open the downloaded file at the param path and gen hashes
         :return: None
         """
-        with open(self.text_file) as f:
-            self.text_data = f.read()
+        try:
+            with open(self.text_file) as f:
+                self.text_data = f.read()
+        except FileNotFoundError:
+            raise
 
         self._generate_hashes()
 
+    @error_handler
     def _generate_hashes(self) -> None:
         """
         This method will set the buffer size to read from the downloaded file, open the file
@@ -41,14 +49,17 @@ class Hash:
         sha_256 = hashlib.sha256()
         sha_1 = hashlib.sha1()
         md_5 = hashlib.md5()
-
-        with open(self.text_data, 'rb') as f:  # Open the file to read its bytes
-            fb = f.read(BLOCK_SIZE)  # Read from the file. Take in the amount declared above
-            while len(fb) > 0:  # While there is still data being read from the file
-                sha_256.update(fb)  # Update the hash
-                sha_1.update(fb)
-                md_5.update(fb)
-                fb = f.read(BLOCK_SIZE)  # Read the next block from the file
-        self.hash_sha256 = str(sha_256.hexdigest())
-        self.hash_sha1 = str(sha_1.hexdigest())
-        self.hash_md5 = str(md_5.hexdigest())
+        try:
+            with open(self.text_data, 'rb') as f:  # Open the file to read its bytes
+                fb = f.read(BLOCK_SIZE)  # Read from the file. Take in the amount declared above
+                while len(fb) > 0:  # While there is still data being read from the file
+                    sha_256.update(fb)  # Update the hash
+                    sha_1.update(fb)
+                    md_5.update(fb)
+                    fb = f.read(BLOCK_SIZE)  # Read the next block from the file
+        except FileNotFoundError:
+            raise
+        else:
+            self.hash_sha256 = str(sha_256.hexdigest())
+            self.hash_sha1 = str(sha_1.hexdigest())
+            self.hash_md5 = str(md_5.hexdigest())
