06c325eb35e336635e4770e47246b38a896a00f4
Anon4Now <tutkoda@gmail.com>
2022-09-09 20:07:05 -0400

    add comments to file

--- resources/vt_check.py
+++ resources/vt_check.py
@@ -1,18 +1,40 @@
-import requests
+"""Module containing class and methods to parse the Virus Total API call"""
+
+# Standard Library imports
 import json
 import datetime
 from dataclasses import dataclass, field
 
+# Third-party imports
+import requests
+
 
 @dataclass
 class VirusTotal:
+    """Data-oriented class that holds the data around the
+    Virus Total API calls.
+    :param hash_sha256: (required) The string representation of the SHA256 hash
+    :param api_endpoint: (required) The string representation Virus Total API endpoint to be called (env var)
+    :param api_key: (required) The string representation Virus Total API key (env var)
+    :param api_key_val: (required) The string representation of the Virus Total API key value (env var)
+    """
     hash_sha256: str
     api_endpoint: str
     api_key: str
     api_key_val: str
     out_dict: dict = field(default_factory=dict, init=False, repr=False)
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
+        """
+        This post init check the API response to make sure there was no error.
+        1. If no error found in response:
+            1a. It will parse the response from the API for the "Last Analysis Time" and the "Last Analysis Stats"
+            1b. It will update the instance dictionary with the data from the response
+        2. If there is an error in response:
+            2a. It will parse the response from the API for the "Error Code"
+            2b. It will update the instance dictionary with the data from the response
+        :return: None
+        """
         if not self.response.get('error'):
             _analysis_date = datetime.datetime.fromtimestamp(
                 self.response.get('data').get('attributes').get('last_analysis_date'))
@@ -25,6 +47,10 @@ class VirusTotal:
 
     @property
     def response(self) -> dict:
+        """
+        This property method will make the API call and the loads response string
+        :return: Dictionary containing the API response
+        """
         # See if file has already been uploaded
         url = f"{self.api_endpoint}{self.hash_sha256}"
 
